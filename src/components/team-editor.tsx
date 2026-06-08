748d2477473136b26606cf34a0349b4e7c0138bb
junleynes <47875283+junleynes@users.noreply.github.com>
2026-06-08 13:17:16 +0800

    Add files via upload

--- src/components/team-editor.tsx
+++ src/components/team-editor.tsx
@@ -352,7 +352,7 @@ export function TeamEditor({ isOpen, setIsOpen, employee, onSave, isPasswordRese
                             render={({ field }) => (
                                 <FormItem>
                                 <FormLabel>Group</FormLabel>
-                                 <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                 <Select onValueChange={field.onChange} value={field.value ?? ''}>
                                     <FormControl>
                                         <SelectTrigger>
                                             <SelectValue placeholder="Select a group" />
@@ -374,7 +374,7 @@ export function TeamEditor({ isOpen, setIsOpen, employee, onSave, isPasswordRese
                             render={({ field }) => (
                             <FormItem>
                                 <FormLabel>Role</FormLabel>
-                                 <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                 <Select onValueChange={field.onChange} value={field.value ?? 'member'}>
                                     <FormControl>
                                     <SelectTrigger>
                                         <SelectValue placeholder="Select a role" />

778d3d24d99366ee3ec60b5f8b60aa93cf56b2a7
junleynes <47875283+junleynes@users.noreply.github.com>
2026-06-30 15:08:03 +0800

    Update google-sheet-sync-dialog.tsx

--- src/components/google-sheet-sync-dialog.tsx
+++ src/components/google-sheet-sync-dialog.tsx
@@ -22,7 +22,7 @@ import type { ShiftTemplate } from './shift-editor';
 import type { LeaveTypeOption } from './leave-type-editor';
 import { saveAs } from 'file-saver';
 
-const DEFAULT_FILTERS = ['1', 'POST PRODUCTION', 'MAMS SUPPORT', 'MEDIA SERVER SUPPORT','1YDl3DNXALbcw-m1i0Gs4HZLge5M2jITe'];
+const DEFAULT_FILTERS = ['1', 'POST PRODUCTION', 'MAMS SUPPORT', 'MEDIA SERVER SUPPORT','TECHNICAL AND MEDIA SERVER SUPPORT DIVISION'];
 
 type GoogleSheetSyncDialogProps = {
   isOpen: boolean;
